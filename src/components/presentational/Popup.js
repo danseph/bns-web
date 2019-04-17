79ac2650be5dc537c63bb6452d56084194c7b058
FrontJonny <agger2154@gmail.com>
2019-04-17 17:21:26 +0900

    changed image

--- src/components/presentational/Popup.js
+++ src/components/presentational/Popup.js
@@ -8,7 +8,7 @@ class Popup extends Component {
                 <div className="popup">
                     <a href="javascript:void(0)" className="btn-x" role="button" onClick={this.props.handlePopupClose}/>
                     <div className="popup-img">
-                        <img src={require("../../images/pop_txt_web_v2.png")} alt="meet up" className="web"/>
+                        <img src={require("../../images/pop_txt_web_v3.png")} alt="meet up" className="web"/>
                         <img src={require("../../images/pop_txt_mobile.png")} alt="meet up" className="mobile"/>
                     </div>
                     <div className="popup-check btn_close">
